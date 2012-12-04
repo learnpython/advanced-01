672d05ad641f7cd627a15d8eeecf0235e79b9689
Aleksanr Zavatskiy <a.zavatskiy@gmail.com>
2012-12-04 21:40:17 +0200

    some drafts

--- zavatskiy/server01.py
+++ zavatskiy/server01.py
@@ -4,10 +4,8 @@ import socket
 class Server01:
 
     def __init__(self, host='127.0.0.1', port=6666, max_queue=1):
-        self.host = host
-        self.port = port
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.socket.bind((self.host, self.port))
+        self.socket.bind((host, port))
         self.socket.listen(max_queue)
 
     def serve(self):
@@ -20,3 +18,7 @@ class Server01:
 
     def shutdown(self):
         self.socket.close()
+
+
+if __name__ == '__main__':
+    Server01()
