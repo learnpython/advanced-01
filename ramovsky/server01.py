3a29a93ac623f758794962366557645ab92e813f
Oleg <o.baranovsky@insollo.com>
2012-12-06 23:36:28 +0200

    thread version

--- ramovsky/server01.py
+++ ramovsky/server01.py
@@ -1,4 +1,6 @@
 import socket
+import signal
+from threading import Thread
 from argparse import ArgumentParser
 
 
@@ -13,30 +15,46 @@ def get_options():
     return ap
 
 
+class Loop(Thread):
+
+    def __init__(self, srv, conn, addr):
+        self.conn = conn
+        self.addr = addr
+        self.srv = srv
+        super().__init__()
+
+    def run(self):
+        print('Connected by', self.addr)
+        while self.srv.running:
+            packet = self.conn.recv(1024)
+            print('packet', packet)
+            if not packet: break
+            cmd, *data = packet.split(b'\n')
+            self.conn.sendall(packet)
+            if cmd == 'quit':
+                self.srv.running = False
+        self.conn.close()
+            
+
 class Server:
 
     def __init__(self, host, port):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.bind((host, port))
+        self.running = True
+        self.clients = []
         
     def run(self):
         while True:
             self.socket.listen(1)
             conn, addr = self.socket.accept()
-            cmd, *data = self.read(conn, addr)
-            if cmd == 'quit':
-                break
+            cli = Loop(self, conn, addr)
+            cli.start()
+            self.clients.append(cli)
 
-    def read(self, conn, addr):
-        print('Connected by', addr)
-        while True:
-            packet = conn.recv(1024)
-            print('packet', packet)
-            if not packet: break
-            cmd, *data = packet.split(b'\n')
-            conn.sendall(packet)
-        conn.close()
-        return cmd, data            
+    def kill(self, signum, frame):
+        print('Signal handler called with signal', signum)
+        self.running = False
 
 
 if __name__ == '__main__':
@@ -44,3 +62,4 @@ if __name__ == '__main__':
     options = ap.parse_args()
     server = Server(options.host, options.port)
     server.run()
+    signal.signal(signal.SIGINT, server.kill)
